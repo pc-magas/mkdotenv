5e9692453943c41e74331505ede143130a8923f9
pcmagas <pcmagas@disroot.org>
2025-03-03 22:12:58 +0200

    Upload PPA.

--- ppa/create.sh
+++ ppa/create.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+# PAth config
+
 SCRIPTPATH=$(dirname "$0") 
 VERSION=$(grep 'const VERSION' ./mkdotenv/msg/msg.go | sed -E 's/.*"([^"]+)".*/\1/')
-
 SRC_FOLDER=${SCRIPTPATH}/mkdotenv_${VERSION}
+TAR_PATH=${SCRIPTPATH}/../../mkdotenv_${VERSION}.orig.tar.gz
+CHANGES_FILE=${SCRIPTPATH}/../../mkdotenv_*_source.changes
 
-TAR_PATH=${SCRIPTPATH}/../mkdotenv_${VERSION}.orig.tar.gz
+
+# PPA distro config
+LINUX_DIST="ubuntu"
+DISTROS=("jammy" "noble")
+DIST=jammy
+
 
 mkdir -p ${SRC_FOLDER}
 cp -r ${SCRIPTPATH}/../mkdotenv ${SRC_FOLDER}
@@ -13,3 +21,16 @@ cp -r ${SCRIPTPATH}/../man ${SRC_FOLDER}
 cp ${SCRIPTPATH}/../Makefile ${SRC_FOLDER}
 cp ${SCRIPTPATH}/../LICENCE ${SRC_FOLDER}
 tar --exclude=debian --exclude=alpinebuild -czf ${TAR_PATH} ${SRC_FOLDER}
+
+for distro in "${DISTROS[@]}"; do
+    echo "Create source package for: "${distro}
+
+    sed -i "s/unstable/${distro}/g" debian/changelog
+	sed -i 's/debian/ubuntu/g' debian/changelog
+	dpkg-buildpackage -S -sa
+	sed -i "s/${distro}/unstable/g" debian/changelog
+	sed -i 's/ubuntu/debian/g' debian/changelog
+done
+
+dput ppa:pcmagas/mkdotenv ${CHANGES_FILE}
+
