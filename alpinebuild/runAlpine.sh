5e14378f4b86654d8cfb9ce645cb344f23d43952
pcmagas <pcmagas@disroot.org>
2025-03-18 23:09:53 +0200

    Remove spaces.

--- alpinebuild/runAlpine.sh
+++ alpinebuild/runAlpine.sh
@@ -26,6 +26,5 @@ tar -tzf ${TARGZ}
 
 sed -i '/^source="\$pkgname-\$pkgver.tar.gz::https:\/\/github.com\/pc-magas\/mkdotenv\/archive\/refs\/tags\/v\$pkgver.tar.gz"/d' ${APKBUILD_OVERLAY}
 
-
 docker build -f ${SCRIPT_DIR}/Dockerfile -t pcmagas/alpinebuild ${SCRIPT_DIR}
 docker run  -v ${OVERLAY}:/home/packager -ti pcmagas/alpinebuild bash
