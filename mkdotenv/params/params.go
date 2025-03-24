bed65a73c3f3994ee9020ee51ba158b510a04bbe
pcmagas <pcmagas@disroot.org>
2025-03-24 21:11:06 +0200

    Check Index out of Bounds.

--- mkdotenv/params/params.go
+++ mkdotenv/params/params.go
@@ -5,7 +5,10 @@ import(
 	"strings"
 	"slices"
 	"mkdotenv/msg"
+	"errors"
 )
+
+var FLAG_ARGUMENTS=[8]string{"--env-file","--input-file","--output-file","-v","--version","-h","--h","--help"}
 
 
 func GetParameters(osArguments []string,errorHandle func(msg string))(string,string,string,string){
@@ -18,15 +21,16 @@ func GetParameters(osArguments []string,errorHandle func(msg string))(string,str
     var variable_name string = osArguments[1]
 	var variable_value string = osArguments[2]
 	var output_file string = ""
+	var err error=nil
 
 	if(strings.HasPrefix(variable_name,"-")){
 		errorHandle("Variable Name should not start with - or --")
+		return dotenv_filename,output_file,variable_name,variable_value
 	}
 
-	ARGUMENTS:= []string{"--env-file","--input-file","--output-file","-v","--version","-h","--h","--help"}
-
-	if(slices.Contains(ARGUMENTS[:],variable_value)){
-		errorHandle("\nVariable value should not contain any of the values:\n"+strings.Join(ARGUMENTS[:],"\n"))
+	if(slices.Contains(FLAG_ARGUMENTS[:],variable_value)){
+		errorHandle("\nVariable value should not contain any of the values:\n"+strings.Join(FLAG_ARGUMENTS[:],"\n"))
+		return dotenv_filename,output_file,variable_name,variable_value
 	}
 
 	for i, arg := range osArguments[3:] {
@@ -38,10 +42,15 @@ func GetParameters(osArguments []string,errorHandle func(msg string))(string,str
 		 	case "--input-file":
 				fallthrough;
 			case "--env-file":
-				dotenv_filename = getValue(value,i,3,osArguments)
+				err,dotenv_filename = getValue(value,i,3,osArguments)
 				
 			case "--output-file":
-				output_file = getValue(value,i,3,osArguments)
+				err,output_file = getValue(value,i,3,osArguments)
+		}
+
+		if(err!=nil){
+			errorHandle("Unable to find the argumewnt value")
+			return dotenv_filename,output_file,variable_name,variable_value
 		}
 	}
 	
@@ -85,13 +94,24 @@ func sliceArgument(argument string) (string, string) {
 	return arguments[0], ""
 }
 
-func getValue(value string,i int,offset int,arguments []string)(string){
+func getValue(value string,i int,offset int,arguments []string)(error,string){
 
-	if(value == ""){
-		// Arguments are parsed with an offset we get the next item + offset
-		return arguments[i+offset+1]
+
+	if(slices.Contains(FLAG_ARGUMENTS[:],value)){
+		return errors.New("Value contains argumwent value"),value
 	}
 
-	return value
+	if(value == ""){
+		index:= i+offset+1
+		if(index >= len(arguments) ){
+			return errors.New("Index out of bounds"),value
+		}
+		// Arguments are parsed with an offset we get the next item + offset
+		return nil,arguments[index]
+
+	}
+
+
+	return nil,value
 }
 
