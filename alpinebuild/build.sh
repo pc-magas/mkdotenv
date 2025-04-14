0cd6e58187f8a07a332bc59ce99f72e5d04e0673
pcmagas <pcmagas@disroot.org>
2025-04-14 23:15:45 +0300

    Build apk file.

--- alpinebuild/build.sh
+++ alpinebuild/build.sh
@@ -1,7 +1,35 @@
 #!/usr/bin/env bash
 
+set -eu
+
+mkdir -p  /home/packager/packages//home
+
+# Append it only once
+grep -qxF 'PACKAGER_REPODEST=/home/packager/packages' ~/.abuild/abuild.conf || \
+echo 'PACKAGER_REPODEST=/home/packager/packages' >> ~/.abuild/abuild.conf
+
+keyfile=$(grep '^PACKAGER_PRIVKEY=' ~/.abuild/abuild.conf | cut -d= -f2 | tr -d '"')
+
+# Make sure it ends with .rsa and the pub file exists
+pubkey="${keyfile}.pub"
+
+if [ -f "$pubkey" ]; then
+    echo "Copying $pubkey to /etc/apk/keys/"
+    sudo cp "$pubkey" /etc/apk/keys/
+else
+    echo "ERROR: Public key not found: $pubkey"
+    exit 1
+fi
+
+echo "Cleanup"
 abuild clear
 abuild clean
+sleep 5
+echo "Fix Checksum"
 abuild checksum
-abuild build
-abuild -r
+echo "Installing Dependencies"
+sleep 5
+abuild deps
+echo "Final Build"
+sleep 10
+abuild -r -K -P "/home/$USER"
