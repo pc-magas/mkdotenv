6f8d4bb232b8ad9761f0c73c3bd75f1e598305a9
pcmagas <pcmagas@disroot.org>
2025-04-17 16:36:37 +0300

    test apk installation

---
+++ alpinebuild/test_apk.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+VERSION=$(cat ${SCRIPT_DIR}/../VERSION)
+APKFILE=mkdotenv-${VERSION}-r0.apk
+APKFILE_FULL=${SCRIPT_DIR}/apkbuild-overlay/home/x86_64/${APKFILE}
+
+if [ -z ${APKFILE_FULL} ]; then
+    bash ${SCRIPT_DIR}/make_apk.sh
+fi
+
+docker run \
+  -v ${APKFILE_FULL}:/root/${APKFILE} \
+  -w /root \
+  alpine \
+  sh -c "apk add --allow-untrusted ${APKFILE} && mkdotenv -h"
