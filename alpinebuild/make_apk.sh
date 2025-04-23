f287f358fc96fc44aa87b4566fd67c7d8b37de49
pcmagas <pcmagas@disroot.org>
2025-04-23 23:51:56 +0300

    Improve image building + release

--- alpinebuild/make_apk.sh
+++ alpinebuild/make_apk.sh
@@ -3,11 +3,18 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 VERSION=$(cat ${SCRIPT_DIR}/../VERSION)
 
+VOLUME_DIR=${SCRIPT_DIR}/volumes
+
+mkdir -p ${VOLUME_DIR}
+
 echo ${VERSION}
 # Path that APKBUILD is Overriden 
-OVERLAY=${SCRIPT_DIR}/apkbuild-overlay
-
+OVERLAY=${VOLUME_DIR}/apkbuild-overlay
+ABUILD_VOLUME=${VOLUME_DIR}/abuild
+RELEASE_DIR=${SCRIPT_DIR}/release
 mkdir -p ${OVERLAY}
+mkdir -p ${ABUILD_VOLUME}
+mkdir -p ${RELEASE_DIR}
 
 # TARBALL name and path  
 TARGZ_NAME=mkdotenv-${VERSION}.tar.gz
@@ -29,4 +36,5 @@ tar -tzf ${TARGZ}
 sed -i '/^source="\$pkgname-\$pkgver.tar.gz::https:\/\/github.com\/pc-magas\/mkdotenv\/archive\/refs\/tags\/v\$pkgver.tar.gz"/d' ${APKBUILD_OVERLAY}
 
 docker build -f ${SCRIPT_DIR}/Dockerfile -t pcmagas/alpinebuild ${SCRIPT_DIR}
-docker run  -v ${OVERLAY}:/usr/src/apkbuild -ti pcmagas/alpinebuild bash
+
+docker run -v ${OVERLAY}:/usr/src/apkbuild -v ${ABUILD_VOLUME}:/home/packager/.abuild -v ${VOLUME_DIR}/keys:/etc/apk/keys -v ${RELEASE_DIR}:/home/packager/release -ti pcmagas/alpinebuild bash
