f89c861acd4f2aa000dedcc17cdf4ed497fa9bcc
pcmagas <pcmagas@disroot.org>
2025-05-15 23:51:53 +0300

    Fix this.

--- aur/testbuild_local.sh
+++ aur/testbuild_local.sh
@@ -22,7 +22,7 @@ ls -l ${VOLUME_DIR}
 
 LANG=C sed "s/source=.*/source=(\"${TARGZ_NAME}\")/" ${SCRIPT_DIR}/PKGBUILD > ${PKGBUILD_LOCAL}
 
-docker build -f ${SCRIPT_DIR}/Dockerfile -t pcmagas/arch-pkg-builder .
+docker build -f ${SCRIPT_DIR}/Dockerfile -t pcmagas/arch-pkg-builder ${SCRIPT_DIR}
 
 docker run --rm -v "${VOLUME_DIR}":/build -w /build pcmagas/arch-pkg-builder build_n_run
 
