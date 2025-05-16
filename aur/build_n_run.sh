77e2ce941ffe71f31730b82c3268843761315e3f
pcmagas <pcmagas@disroot.org>
2025-05-16 21:05:57 +0300

    Attempt to fix permission.

--- aur/build_n_run.sh
+++ aur/build_n_run.sh
@@ -3,6 +3,8 @@
 # Script intented to run *INSIDE* a docker container ti builds and runs mkdotenv inside an arch linux container
 set -euo pipefail
 
+sudo chown -R $(whoami) /build
+
 updpkgsums 
 makepkg -si --noconfirm 
 mkdotenv -h
