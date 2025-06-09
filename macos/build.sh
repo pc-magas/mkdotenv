f1115563c4289db1b662061682eeae83f0477df4
pcmagas <pcmagas@disroot.org>
2025-06-09 23:24:55 +0300

    Test build.

--- macos/build.sh
+++ macos/build.sh
@@ -8,7 +8,16 @@ ZIP_PATH="${SCRIPTPATH}/mkdotenv-macos.zip"
 echo ${ZIP_PATH}
 
 rm -rf ${ZIP_PATH}
-make bin OS=darwin ARCH=arm64 COMPILED_BIN_PATH="/tmp/mkdotenv"
+
+OS_TYPE="$(uname)"
+if [[ "$OS_TYPE" == "Darwin" ]]; then
+  echo "Building Natively on macOS"
+  make bin COMPILED_BIN_PATH="/tmp/mkdotenv"
+else
+  echo "Buolding for MacOs from ${OS_TYPE}"
+  make bin OS=darwin ARCH=arm64 COMPILED_BIN_PATH="/tmp/mkdotenv"
+fi
+
 
 mkdir  -p ${SCRIPTPATH}/bin
 
@@ -17,13 +26,18 @@ zip -j -o ${ZIP_PATH} ${SCRIPTPATH}/bin/mkdotenv
 
 SHA256=$(shasum -a 256 ${ZIP_PATH} | awk '{ print $1 }')
 
+echo "Fixing Checksum upon ${FORMULA_PATH}"
 sed -i -E "s|sha256 \".*\"|sha256 \"${SHA256}\"|" "${FORMULA_PATH}"
 echo "Updated SHA256 in ${FORMULA_PATH}"
 
 
-FORMULA_TEST_PATH="${SCRIPTPATH}/bin/mkdotenv.rb"
-cp ${FORMULA_PATH} ${FORMULA_TEST_PATH}
+if [[ "$OS_TYPE" == "Darwin" ]]; then
 
-ls -l  ${FORMULA_TEST_PATH}
+  echo "[MAC] Modifying Formula for local installation"
 
-sed -i -E "s|url.*|url \"file://${ZIP_PATH}\"|" ${FORMULA_TEST_PATH}
+  FORMULA_TEST_PATH="${SCRIPTPATH}/bin/mkdotenv.rb"
+  cp ${FORMULA_PATH} ${FORMULA_TEST_PATH}
+  ls -l  ${FORMULA_TEST_PATH}
+  sed -i -E "s|url.*|url \"file://${ZIP_PATH}\"|" ${FORMULA_TEST_PATH}
+
+fi
