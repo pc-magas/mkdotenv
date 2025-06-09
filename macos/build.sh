aa37a58659129d2f0e66fc5fe29c23c842dfdb4c
pcmagas <pcmagas@disroot.org>
2025-06-09 18:11:58 +0300

    Release for MacOS

--- macos/build.sh
+++ macos/build.sh
@@ -1,5 +1,10 @@
-#!/ust/bin/env bash
+#!/usr/bin/env bash
+
+SCRIPTPATH="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 rm -rf ./mkdotenv-macos.zip
-make compile OS=darwin ARCH=arm64 COMPILED_BIN_PATH="/tmp/mkdotenv"
-zip -j -o mkdotenv-macos.zip /tmp/mkdotenv
+make bin OS=darwin ARCH=arm64 COMPILED_BIN_PATH="/tmp/mkdotenv"
+
+mkdir ${SCRIPTPATH}/bin
+cp ./bin/mkdotenv-darwin-arm64  ${SCRIPTPATH}/bin/mkdotenv
+zip -j -o mkdotenv-macos.zip ${SCRIPTPATH}/bin/mkdotenv
