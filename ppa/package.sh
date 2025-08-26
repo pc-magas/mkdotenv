8dc8a712b44be024e52b8ba032f6ec01918c6899
pcmagas <pcmagas@disroot.org>
2025-08-26 22:10:00 +0300

    Build PPA.

--- ppa/package.sh
+++ ppa/package.sh
@@ -22,13 +22,14 @@ DIST=jammy
 bash ${SCRIPTPATH}/make_tar.sh
 
 cd ${SCRIPTPATH}/..
-
+pwd
+sleep 10
 for distro in "${DISTROS[@]}"; do
     echo "Create source package for: "${distro}
 
     sed -i "s/unstable/${distro}/g" ${SCRIPTPATH}/../debian/changelog
 	sed -i 's/debian/ubuntu/g' ${SCRIPTPATH}/../debian/changelog
 	dpkg-buildpackage -S -sa
-	sed -i "s/${distro}/unstable/g" ${SCRIPTPATH}/debian/changelog
-	sed -i 's/ubuntu/debian/g' ${SCRIPTPATH}/debian/changelog
+	sed -i "s/${distro}/unstable/g" ${SCRIPTPATH}/../debian/changelog
+	sed -i 's/ubuntu/debian/g' ${SCRIPTPATH}/../debian/changelog
 done
