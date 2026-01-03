817e63ca937d6469f505cc74cd1b925b2a397fc0
pcmagas <pcmagas@disroot.org>
2026-01-03 17:57:25 +0200

    Remove Unwanted  library.

--- mkdotenv/params/param_parser_test.go
+++ mkdotenv/params/param_parser_test.go
@@ -469,3 +469,97 @@ func TestParamParser_TestMultipleString(t *testing.T) {
 	assert.True(t,complete)
 	assert.Equal(t,expectedArgs,values.Args)
 }
+
+func TestParamParser_TestMultipleStringWithOtherflags(t *testing.T) {
+	flags := FlagList{
+		{
+			Name:     "argument",
+			Aliases:  []string{"arg"},
+			Short: "a",
+			AllowMultiple: true,
+			Type: StringType,
+			Required: false,
+			DefaultValue: "",
+			Usage:    "TestUsage",
+			Order:    2,
+			Validator: ValidateCommon,
+		},
+		{
+			Name:     "name",
+			Aliases:  []string{"onoma"},
+			Short: "n",
+			AllowMultiple: true,
+			Type: StringType,
+			Required: true,
+			DefaultValue: "",
+			Usage:    "TestUsage",
+			Order:    2,
+			Validator: ValidateCommon,
+		},
+	}
+
+	values:=testArgs{
+		Args:[]string{},
+	}
+
+	expectedArgs:=[]string{"1","2","3","4"}
+
+	parser := NewParamParser[testArgs](flags)
+	parser.OnAssign = func(meta FlagMeta, value string, args *testArgs) error {
+		switch meta.Name {
+			case "argument":
+				args.Args=append(args.Args,value)
+		}
+		return nil
+	}
+
+	args := []string{"executable","--argument","1","--arg","2","-a","3","--name=alexandros","-a","4"}
+	complete,err:=parser.Parse(args,&values);
+
+	assert.NoError(t,err)
+	assert.True(t,complete)
+	assert.Equal(t,expectedArgs,values.Args)
+}
+
+func TestParamParser_ValidatorFails(t *testing.T) { 
+	flags := FlagList{
+		{
+			Name:     "argument",
+			Aliases:  []string{"arg"},
+			Short: "a",
+			AllowMultiple: false,
+			Type: StringType,
+			Required: false,
+			DefaultValue: "",
+			Usage:    "TestUsage",
+			Order:    2,
+			Validator:  func(value string) bool {
+				return false
+			},
+		},
+	}
+
+	values:=testArgs{
+		Args:[]string{},
+	}
+
+	expectedArgs:=[]string{}
+
+	parser := NewParamParser[testArgs](flags)
+	parser.OnAssign = func(meta FlagMeta, value string, args *testArgs) error {
+		switch meta.Name {
+			case "argument":
+				args.Args=append(args.Args,value)
+		}
+		return nil
+	}
+
+	args := []string{"executable","--argument","1"}
+	complete,err:=parser.Parse(args,&values);
+
+	assert.Error(t,err)
+	assert.False(t,complete)
+	assert.Equal(t,expectedArgs,values.Args)
+}
+
+
