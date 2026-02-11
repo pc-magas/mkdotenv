4d0826737062a80c96ddba57deb5f9298d3dcae5
pcmagas <pcmagas@disroot.org>
2026-02-11 22:59:07 +0200

    Output color.

--- intergration_tests/keepassx/run.sh
+++ intergration_tests/keepassx/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color (Reset)
+
+
 SCRIPTPATH="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 SOURCEPATH=${SCRIPTPATH}/../..
 
@@ -14,3 +19,28 @@ echo "Executing"
 echo "#################################"
 
 ${SOURCEPATH}/bin/mkdotenv-linux-amd64 --output-file=.env
+
+# Extract the value for a specific key
+ACTUAL_VALUE=$(grep "^PASSWORD=" .env | cut -d'=' -f2)
+
+echo
+echo "#################################"
+echo "Validating"
+echo "#################################"
+
+
+if [ "$ACTUAL_VALUE" == "1234" ]; then
+    echo -e "${GREEN}PASSWORD is correct.${NC}"
+else
+    echo "${RED}PASSWORD is missing or incorrect.${NC}"
+    exit 1
+fi
+
+ACTUAL_VALUE=$(grep "^USERNAME=" .env | cut -d'=' -f2)
+
+if [ "$ACTUAL_VALUE" == "test" ]; then
+    echo -e "${GREEN}USERNAME is correct.${NC}"
+else
+    echo -e "${RED}USERNAME is missing or incorrect.${NC}"
+    exit 1
+fi
