d71ed17a24fc24a25a8252f6d9f2c98f8198929b
pcmagas <pcmagas@disroot.org>
2026-04-18 18:29:11 +0300

    Build webpage.

--- webpage/build.sh
+++ webpage/build.sh
@@ -7,3 +7,5 @@ cd $SCRIPTPATH
 npm ci
 cp ./node_modules/@pc-magas/asciiart/dist/js/asciiart.min.js ./public/
 cp ./node_modules/@pc-magas/asciiart/dist/css/style.css ./public/
+
+npx @11ty/eleventy
