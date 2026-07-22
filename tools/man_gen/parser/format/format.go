9fc43e9463d07d0abd576f70503e46c11e81cea6
pcmagas <pcmagas@disroot.org>
2026-07-22 15:13:39 +0300

    Format Fields.

--- tools/man_gen/parser/format/format.go
+++ tools/man_gen/parser/format/format.go
@@ -12,7 +12,7 @@ func generateBaseCommand(spec parser.CommandSpec, prefix string, suffix string) 
 		&b,
 		"mkdotenv()::resolve(%s%s%s)::%s(",
 		prefix,
-		spec.ResolvesPath,
+		spec.ValueToResolve.Value,
 		suffix,
 		spec.Command,
 	)
@@ -60,9 +60,21 @@ func GenerateUsage(spec parser.CommandSpec, html bool) []string {
 }
 
 func GenerateDescription(spec parser.CommandSpec, html bool) string {
-	var title_prefix string=".SH",title_suffix string=""
+	var title_prefix string=".SH",title_suffix string="\n"
+	var description_prefix string="",description_suffix string="\n"
 	var content_prefix string="", content_suffix string=""
-	var description_prefix string="",description_suffix string=""
+
+
+	if(html){
+		title_prefix="<h2>"
+		title_suffix="</h2>"
+		
+		content_prefix="<section>"
+		content_suffix="</section>"
+
+		description_prefix="<p>"
+		description_suffix="</p>"
+	}
 
 	var b strings.Builder
 
@@ -74,6 +86,131 @@ func GenerateDescription(spec parser.CommandSpec, html bool) string {
 	b.WriteString(spec.Description)
 	b.WriteString(description_suffix)
 	b.WriteString(content_suffix)
+
+	return b.String()
+}
+
+func GenerateName(spec parser.CommandSpec, html bool) string {
+
+	var section_prefix string="",section_suffix string="\n",title_prefix string=".SH",title_suffix string="\n",paragraph_prefix string="",paragraph_suffix string="";
+	var b strings.Builder
+
+	if(html){
+		section_prefix="<section>"
+		section_suffix="</section>"
+		title_prefix="<h2>"
+		title_suffix="</h2>"
+		paragraph_prefix="<p>"
+		paragraph_suffix="</p>"
+	}
+
+	
+	b.WriteString(section_prefix)
+	b.WriteString(title_prefix)
+	b.WriteString("NAME")
+	b.WriteString(title_suffix)
+	b.WriteString(paragraph_prefix)
+	b.WriteString(spec.Command)
+	b.WriteString(paragraph_suffix)
+	b.WriteString(section_suffix)
+
+	return b.String()
+}
+
+func GenerateParamsDescription(spec parser.CommandSpec,html bool) string {
+
+	if(len(spec.Params) == 0){
+		return "";
+	}
+
+	var section_prefix string="",section_suffix string="\n",title_prefix string=".SH",title_suffix string="\n";
+	var descrption_prefix string="",description_suffix string="";
+	var argument_prefix string=".TP\n.B",argument_suffix="\n",argument_description_prefix string ="", argument_description_prefix string="\n",required_prefix string="", required_suffix string="";
+	
+	var b strings.Builder
+
+	if(html){
+		section_prefix="<section>"
+		section_suffix="</section>"
+		title_prefix="<h2>"
+		title_suffix="</h2>"
+		descrption_prefix="<dl class=\"desc-columns\">"
+		description_suffix="</dl>"
+		argument_prefix="<dt>"
+		argument_suffix="</dt>"
+		argument_description_prefix="<dd>"
+		argument_description_prefix="</dd>"
+	}
+
+	b.WriteString(section_prefix)
+	b.WriteString(title_prefix)
+	b.WriteString("ARGUMENTS")
+	b.WriteString(title_suffix)
+
+	for i, param := range spec.Params {
+		b.WriteString(argument_prefix)
+		b.WriteString(param.Name)
+		b.WriteString(argument_suffix)
+
+		b.WriteString(argument_description_prefix)
+		if(param.Required){
+			b.WriteString(required_prefix)
+			b.WriteString("REQUIRED")
+			b.WriteString(required_suffix)
+			b.WriteString(" ")
+		}
+		b.WriteString(param.Description)
+		b.WriteString(argument_description_prefix)
+	}
+
+	b.WriteString(argument_suffix)
+	b.WriteString(section_suffix)
+
+	return b.String()
+}
+
+func GenerateFieldDescription(spec parser.CommandSpec,html bool) string {
+
+	if(len(spec.Fields) == 0){
+		return "";
+	}
+
+	var section_prefix string="",section_suffix string="\n",title_prefix string=".SH",title_suffix string="\n";
+	var descrption_prefix string="",description_suffix string="";
+	var argument_prefix string=".TP\n.B",argument_suffix="\n",argument_description_prefix string ="", argument_description_prefix string="\n";
+	
+	var b strings.Builder
+
+	if(html){
+		section_prefix="<section>"
+		section_suffix="</section>"
+		title_prefix="<h2>"
+		title_suffix="</h2>"
+		descrption_prefix="<dl class=\"desc-columns\">"
+		description_suffix="</dl>"
+		argument_prefix="<dt>"
+		argument_suffix="</dt>"
+		argument_description_prefix="<dd>"
+		argument_description_prefix="</dd>"
+	}
+
+	b.WriteString(section_prefix)
+	b.WriteString(title_prefix)
+	b.WriteString("FIELDS")
+	b.WriteString(title_suffix)
+
+	for i, param := range spec.Fields {
+		b.WriteString(argument_prefix)
+		b.WriteString(param.Name)
+		b.WriteString(argument_suffix)
+
+		b.WriteString(argument_description_prefix)
+		b.WriteString(param.Description)
+		b.WriteString(argument_description_prefix)
+	}
+
+	b.WriteString(argument_suffix)
+	b.WriteString(section_suffix)
 
 	return b.String()
 }
